2e0edaf45cf5a6e86990cf4cb4d7c5dc599ae337
PedroOrtRec <pedroortegarechi@gmail.com>
2024-12-31 00:40:27 +0100

    [Config][New configurations]
    - Added configurations for features in developer preview status

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,18 +1,21 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
 import {
 	provideClientHydration,
 	withEventReplay,
+  withIncrementalHydration,
 } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
 	providers: [
-		provideZoneChangeDetection({ eventCoalescing: true }),
-		provideRouter(routes),
-		provideClientHydration(withEventReplay()),
+		provideExperimentalZonelessChangeDetection(),
+		provideRouter(routes, withComponentInputBinding()),
+    provideHttpClient(withFetch()),
+		provideClientHydration(withEventReplay(), withIncrementalHydration()),
 		provideAnimationsAsync(),
 	],
 };
